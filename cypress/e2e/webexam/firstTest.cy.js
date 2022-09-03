d09913ad973b614d9139b32b32378724ef6dd6e2
gatecrasher83 <vnike83@gmail.com>
2022-09-03 20:30:31 +0700

    changed name 'it'

--- cypress/e2e/webexam/firstTest.cy.js
+++ cypress/e2e/webexam/firstTest.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress-xpath" />
 
-it("By.ID", () =>{
+it("By.CSS selector", () =>{
     cy.viewport(1440, 900)
     cy.visit('https://www.google.com/')
     cy.get("div[class='FPdoLc lJ9FBc'] input[value='Поиск в Google']")
