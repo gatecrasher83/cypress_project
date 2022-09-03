e3f2217fb3ae1cbf083e50d0462d122c2bf5b83c
gatecrasher83 <vnike83@gmail.com>
2022-09-03 20:50:06 +0700

    delete all code

--- cypress/e2e/webexam/firstTest.cy.js
+++ cypress/e2e/webexam/firstTest.cy.js
@@ -1,7 +1,1 @@
-/// <reference types="cypress-xpath" />
-
-it("By.CSS selector", () =>{
-    cy.viewport(1440, 900)
-    cy.visit('https://www.google.com/')
-    cy.get("div[class='FPdoLc lJ9FBc'] input[value='Поиск в Google']")
-})
+/// <reference types="cypress" />
