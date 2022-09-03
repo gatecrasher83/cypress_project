31f0682ad0d357c0876f51df69a55e51674b9902
gatecrasher83 <vnike83@gmail.com>
2022-09-03 22:32:11 +0700

    added tests contains

--- cypress/e2e/webexam/firstTest.cy.js
+++ cypress/e2e/webexam/firstTest.cy.js
@@ -1,1 +1,23 @@
 /// <reference types="cypress" />
+
+it('',() =>{
+    cy.visit('https://80.69.180.108:8441?lang="en"')
+    cy.contains(' Войти ');
+})
+
+it('',() =>{
+    cy.visit('https://80.69.180.108:8441?lang="en"')
+    cy.contains('span',' Войти ');
+})
+
+it('',() =>{
+    cy.visit('https://80.69.180.108:8441?lang="en"')
+    cy.contains(' войти ', {matchCase: false});
+})
+
+it.only('',() =>{
+    cy.viewport(1440, 900)
+    cy.visit('https://80.69.180.108:8441?lang="en"')
+    cy.get('form').contains('Забыли пароль?')
+    cy.get("div.authorization-small-text>p:nth-child(1)")
+})
