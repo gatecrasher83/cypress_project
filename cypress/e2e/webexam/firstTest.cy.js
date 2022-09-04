73b02164567b7eac8298178e40cfb3c12187efd2
gatecrasher83 <vnike83@gmail.com>
2022-09-04 18:54:48 +0700

    added asseryions tests

--- cypress/e2e/webexam/firstTest.cy.js
+++ cypress/e2e/webexam/firstTest.cy.js
@@ -1,5 +1,35 @@
 /// <reference types="cypress" />
 
 it('Should', () =>{
-    cy.visit('')
+    cy.viewport(1440, 900);
+    cy.visit('https://80.69.180.108:8441?lang="en"');
+    cy.get('input[type="email"]')
+    .type(100, {force: true})
+    .should('have.value', '100')
+    .and("be.not.visible")    
 })
+
+it('Expect', () =>{
+    cy.viewport(1440, 900);
+    cy.visit('https://80.69.180.108:8441?lang="en"');
+    cy.get('input[type="email"]')
+    .type(100, {force: true}).then(input=>{
+        expect(input).to.have.value(100)
+    })   
+})
+
+it('check value', () =>{
+    cy.viewport(1440, 900);
+    cy.visit('https://payment.mts.ru/pay/1150?paymentform=1150&bt=UK&number=&amount=500&channel=1&source=mts&_ga=2.114043463.1161918336.1662288492-1928978872.1650893802');
+    cy.get('input[data-val="ANONYMOUS_CARD"]')
+    .should("be.checked")
+    })   
+
+it.only('check is visible', () =>{
+    cy.viewport(1440, 900);
+    cy.visit('https://autotrenajer.ru/');
+    cy.contains('ТРЕНАЖЕРЫ')
+    .trigger('mouseover')
+    .get('header a[href="/cost"]>span[class="menu-item-text"]')
+    .should("be.visible")
+    })   
