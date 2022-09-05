fecc4f896dd024a95bd0532603273c9b6dd38820
gatecrasher83 <vnike83@gmail.com>
2022-09-05 13:49:34 +0700

    first test

--- cypress/e2e/webexam/firstTest.cy.js
+++ cypress/e2e/webexam/firstTest.cy.js
@@ -1,1 +1,9 @@
 /// <reference types="cypress" />
+
+//type
+
+it('type', ()=>{
+    cy.visit("https://80.69.180.108:8441/")
+        .get('[type="password"]').click()
+        .type('112233344')
+})
