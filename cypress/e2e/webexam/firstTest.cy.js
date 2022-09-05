7f3f9937adb2147c5eec314d50dbfb216ab3e96d
gatecrasher83 <vnike83@gmail.com>
2022-09-05 14:04:21 +0700

    added tests to click action

--- cypress/e2e/webexam/firstTest.cy.js
+++ cypress/e2e/webexam/firstTest.cy.js
@@ -4,6 +4,39 @@
 
 it('type', ()=>{
     cy.visit("https://80.69.180.108:8441/")
-        .get('[type="password"]').click()
-        .type('112233344')
+        .get('[type="password"]')
+        .type('112233344', {force: true})
+})
+
+it('focus', ()=>{
+    cy.visit("https://80.69.180.108:8441/")
+        .get('[type="password"]')
+        .focus()
+})
+
+it('blur', ()=>{
+    cy.visit("https://80.69.180.108:8441/")
+        .get('[type="password"]')
+        .focus()
+        .blur()
+})
+
+it('clear', ()=>{
+    cy.visit("https://80.69.180.108:8441/")
+        .get('[type="password"]')
+        .type('112233344', {force: true})
+        .wait(2000)
+        .clear({force: true})
+})
+
+it('submit', ()=>{
+    cy.visit("https://80.69.180.108:8441/")
+        .get('form[class^=w-50]')
+        .submit()
+})
+
+it.only('click', ()=>{
+    cy.visit("https://80.69.180.108:8441/")
+        .get('a[class="clickable-text"]')
+        .click({force: true})
 })
