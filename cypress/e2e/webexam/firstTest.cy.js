99e28dffcf60b170eed9449c2ece3943b5ba3e7b
gatecrasher83 <vnike83@gmail.com>
2022-09-07 17:58:41 +0700

    c

--- cypress/e2e/webexam/firstTest.cy.js
+++ cypress/e2e/webexam/firstTest.cy.js
@@ -1,42 +1,18 @@
 /// <reference types="cypress" />
+import { enterwebExam } from "../../support/pages/enterWebExam"
+import { basePage } from "../../support/pages/basePage"
 
-//type
-
-it('type', ()=>{
-    cy.visit("https://80.69.180.108:8441/")
-        .get('[type="password"]')
-        .type('112233344', {force: true})
-})
-
-it('focus', ()=>{
-    cy.visit("https://80.69.180.108:8441/")
-        .get('[type="password"]')
-        .focus()
-})
-
-it('blur', ()=>{
-    cy.visit("https://80.69.180.108:8441/")
-        .get('[type="password"]')
-        .focus()
-        .blur()
-})
-
-it('clear', ()=>{
-    cy.visit("https://80.69.180.108:8441/")
-        .get('[type="password"]')
-        .type('112233344', {force: true})
-        .wait(2000)
-        .clear({force: true})
-})
-
-it('submit', ()=>{
-    cy.visit("https://80.69.180.108:8441/")
-        .get('form[class^=w-50]')
-        .submit()
-})
-
-it.only('click', ()=>{
-    cy.visit("https://80.69.180.108:8441/")
-        .get('a[class="clickable-text"]')
-        .click({force: true})
+it('assertion footer', ()=>{
+    cy.viewport(1600,900)
+    basePage.open("https://80.69.180.108:8441/")    
+    basePage.typeEnter('super@admin.ru', 'qwe123QWE!@#')
+    cy.wait(3000)
+    cy.screenshot()
+    enterwebExam.openContragent()
+    cy.screenshot()
+    enterwebExam.scrollToPaginator()
+    cy.wait(3000)
+    cy.screenshot()
+    enterwebExam.footerAssertion(' ©2000-2022 Форвард ')
+    basePage.unAuthorize() 
 })
