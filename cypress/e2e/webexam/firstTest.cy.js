3f617aa3dfbaa013c4389ca502bbf7653ce62721
gatecrasher83 <vnike83@gmail.com>
2022-09-07 13:49:00 +0700

    edit

--- cypress/e2e/webexam/firstTest.cy.js
+++ cypress/e2e/webexam/firstTest.cy.js
@@ -3,15 +3,15 @@ import { enterwebExam } from "../../support/pages/enterWebExam"
 import { basePage } from "../../support/pages/basePage"
 
 it('assertion footer', ()=>{
-    cy.viewport(1440,900)
+    cy.viewport(1600,900)
     basePage.open("https://80.69.180.108:8441/")    
     basePage.typeEnter('super@admin.ru', 'qwe123QWE!@#')
-    cy.wait(2000)
+    cy.wait(3000)
     cy.screenshot()
     enterwebExam.openContragent()
     cy.screenshot()
     enterwebExam.scrollToPaginator()
-    cy.wait(2000)
+    cy.wait(3000)
     cy.screenshot()
     enterwebExam.footerAssertion(' ©2000-2022 Форвард ')
     basePage.unAuthorize() 
