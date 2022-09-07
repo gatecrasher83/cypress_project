2ceb86375497e7aecda8db3ae41b97220d73376c
gatecrasher83 <vnike83@gmail.com>
2022-09-07 14:05:20 +0700

    start api

--- cypress/e2e/webexam/firstTest.cy.js
+++ cypress/e2e/webexam/firstTest.cy.js
@@ -1,1 +1,14 @@
 /// <reference types="cypress" />
+
+context('Network Requests', () => {
+    beforeEach(() => {
+      cy.visit('https://80.69.180.108:8441')
+    })
+
+    it("Example sending the GET request"), ()=>{
+        cy.request("https://80.69.180.108:8441")
+            .then((response)=>{
+                console.log(response);
+        })
+}
+})
